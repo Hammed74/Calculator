1a48b9dda51349e8d42c7f141c1058940a386e58
Rarri74 <Hammed74@gmail.com>
2023-10-25 17:03:01 -0400

    created calculator and light style with css

--- index.js
+++ index.js
@@ -1,23 +1,92 @@
-function add(x,y){
-    return x + y
+const plus = document.querySelector(".plus");
+const clear = document.querySelector(".clear")
+const equal = document.querySelector(".equals");
+const numberButtons = document.querySelectorAll(".button");
+const screenLast = document.querySelector(".screen-last")
+const screenCurrent = document.querySelector(".screen-current");
+const operatorButtons = document.querySelectorAll(".operator");
+
+let firstOperand = ""
+let secondOperand = ""
+let currentOperation = null
+let shouldResetScreen = false
+
+equal.addEventListener("click", evaluate)
+clear.addEventListener("click", clearScreen)
+
+numberButtons.forEach(button => {
+    button.addEventListener("click", () => appendNumber(button.textContent))
+})
+
+operatorButtons.forEach(button => {
+    button.addEventListener("click", () => setOperation(button.textContent))
+})
+
+function appendNumber(number){
+    if(shouldResetScreen === true){
+        resetScreen()
+    }
+    screenCurrent.textContent += number
+    shouldResetScreen = false
 }
 
-function subtract(x,y){
-    return x - y
+function setOperation(operator){
+    if (currentOperation !== null){
+        evaluate()
+    }
+    firstOperand = screenCurrent.textContent
+    currentOperation = operator
+    screenLast.textContent = `${firstOperand} ${currentOperation}`
+    shouldResetScreen = true
 }
 
-function multiply(x,y){
-    return x*y
+function evaluate(){
+    secondOperand = screenCurrent.textContent
+    screenLast.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`
+    screenCurrent.textContent = operate(currentOperation, firstOperand, secondOperand)
+    currentOperation = null
+
 }
 
-function divide(x,y){
-    return x/y
+function clearScreen(){
+    screenCurrent.textContent = ""
+    screenLast.textContent = ""
+    firstOperand = ""
+    secondOperand = ""
+    currentOperation = null
 }
 
-
-
-function operate(x,y){
-   return multiply(x,y)
+function resetScreen(){
+    screenCurrent.textContent = ""
 }
 
-console.log(operate(5,5))
+function add(a,b){
+    return a + b
+}
+
+function multiply(a,b){
+    return a *b
+}
+
+function subtract(a,b){
+    return a - b
+}
+
+function divide(a,b){
+    return a/b
+}
+
+function operate(operator,a,b){
+    a = Number(a);
+    b = Number(b);
+    switch(operator){
+        case "+":
+            return add(a,b);
+        case "x":
+            return multiply(a,b);
+        case "-":
+            return subtract(a,b);
+        case "÷":
+            return divide(a,b);
+    }
+}
