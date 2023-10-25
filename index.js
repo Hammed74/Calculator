973910a62fa35b6a6214c93eae5a852784587365
Rarri74 <Hammed74@gmail.com>
2023-10-25 17:34:21 -0400

    fixed bug with hitting equal while value is zero

--- index.js
+++ index.js
@@ -1,92 +1,98 @@
 const plus = document.querySelector(".plus");
-const clear = document.querySelector(".clear")
+const clear = document.querySelector(".clear");
 const equal = document.querySelector(".equals");
 const numberButtons = document.querySelectorAll(".button");
-const screenLast = document.querySelector(".screen-last")
+const screenLast = document.querySelector(".screen-last");
 const screenCurrent = document.querySelector(".screen-current");
 const operatorButtons = document.querySelectorAll(".operator");
 
-let firstOperand = ""
-let secondOperand = ""
-let currentOperation = null
-let shouldResetScreen = false
+let firstOperand = "";
+let secondOperand = "";
+let currentOperation = null;
+let shouldResetScreen = false;
+screenCurrent.textContent = 0;
 
-equal.addEventListener("click", evaluate)
-clear.addEventListener("click", clearScreen)
+equal.addEventListener("click", evaluate);
+clear.addEventListener("click", clearScreen);
 
-numberButtons.forEach(button => {
-    button.addEventListener("click", () => appendNumber(button.textContent))
-})
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => appendNumber(button.textContent));
+});
 
-operatorButtons.forEach(button => {
-    button.addEventListener("click", () => setOperation(button.textContent))
-})
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", () => setOperation(button.textContent));
+});
 
-function appendNumber(number){
-    if(shouldResetScreen === true){
-        resetScreen()
-    }
-    screenCurrent.textContent += number
-    shouldResetScreen = false
+function appendNumber(number) {
+  if (shouldResetScreen === true || screenCurrent.textContent == 0) {
+    resetScreen();
+  }
+  screenCurrent.textContent += number;
+  shouldResetScreen = false;
 }
 
-function setOperation(operator){
-    if (currentOperation !== null){
-        evaluate()
-    }
-    firstOperand = screenCurrent.textContent
-    currentOperation = operator
-    screenLast.textContent = `${firstOperand} ${currentOperation}`
-    shouldResetScreen = true
+function setOperation(operator) {
+  if (currentOperation !== null) {
+    evaluate();
+  }
+  firstOperand = screenCurrent.textContent;
+  currentOperation = operator;
+  screenLast.textContent = `${firstOperand} ${currentOperation}`;
+  shouldResetScreen = true;
 }
 
-function evaluate(){
-    secondOperand = screenCurrent.textContent
-    screenLast.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`
-    screenCurrent.textContent = operate(currentOperation, firstOperand, secondOperand)
-    currentOperation = null
-
+function evaluate() {
+  if (screenCurrent.textContent !== 0 && currentOperation !== null) {
+    secondOperand = screenCurrent.textContent;
+    screenLast.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`;
+    screenCurrent.textContent = operate(
+      currentOperation,
+      firstOperand,
+      secondOperand
+    );
+    currentOperation = null;
+  }
 }
 
-function clearScreen(){
-    screenCurrent.textContent = ""
-    screenLast.textContent = ""
-    firstOperand = ""
-    secondOperand = ""
-    currentOperation = null
+function clearScreen() {
+  screenCurrent.textContent = 0;
+  screenLast.textContent = "";
+  firstOperand = "";
+  secondOperand = "";
+  currentOperation = null;
 }
 
-function resetScreen(){
-    screenCurrent.textContent = ""
+function resetScreen() {
+  screenCurrent.textContent = "";
 }
 
-function add(a,b){
-    return a + b
+function add(a, b) {
+  return a + b;
 }
 
-function multiply(a,b){
-    return a *b
+function multiply(a, b) {
+  return a * b;
 }
 
-function subtract(a,b){
-    return a - b
+function subtract(a, b) {
+  return a - b;
 }
 
-function divide(a,b){
-    return a/b
+function divide(a, b) {
+  return a / b;
 }
 
-function operate(operator,a,b){
-    a = Number(a);
-    b = Number(b);
-    switch(operator){
-        case "+":
-            return add(a,b);
-        case "x":
-            return multiply(a,b);
-        case "-":
-            return subtract(a,b);
-        case "÷":
-            return divide(a,b);
-    }
+function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "x":
+      return multiply(a, b);
+    case "-":
+      return subtract(a, b);
+    case "÷":
+      return divide(a, b);
+  }
 }
